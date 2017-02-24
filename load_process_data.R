95826ce51fedff61322c63a569edbbd3c27c9075
Eric Chang <ericchang00@gmail.com>
2017-02-24 16:19:33 -0600

    add data augmentation func

--- load_process_data.R
+++ load_process_data.R
@@ -48,16 +48,31 @@ process_data <- function(dat, is_training_data){
 }
 
 
-augment_data <- function(dat, train, n_events){
+augment_data <- function(dat, train, max_count_events=100){
     # generates more training data by recalculating features
     # on ids with n occured events by removing the nth observation 
     # and setting event[n-1] as response. 
     # iteratively "shaves" dataset down from max n_events to ids with only 2 events
-    out <- data.frame(names(train))
     
+    out <- train[0,]  # create empty dataframe with same schema as train
+    valid_ids <- train %>%   # a valid id is one with count_events > 2 (and less than the max)
+        filter((count_events > 2) & (count_events < max_count_events)) 
+        
+    while (max(valid_ids$count_events) > 2){
+        dat <- filter(dat, id %in% valid_ids$id)
+        
+        last_events_to_remove <- dat %>% group_by(id) %>% summarise(last_timestamp = last(timestamp))
+        dat <- filter(dat, !(timestamp %in% last_events_to_remove$last_timestamp))
+        
+        valid_ids <- process_data(dat, is_training_data = TRUE)
+        out <- out %>% bind_rows(valid_ids)
+        
+        message("Shaving event number: ", max(valid_ids$count_events))
+    }
     
-
+    return(out)
 }
+
 
 # MAIN
 # To create training and testing dataset
@@ -74,20 +89,10 @@ test_raw <- test_raw %>% left_join(filter(train_raw, id %in% test_raw$id))
 train_raw <- filter(train_raw, !(id %in% test_raw$id)) %>% mutate(id = as.character(id))
 assert_that(all(!train_raw$id %in% test_raw$id))  # assert no overlap between training and test sets
 
-# features engineering
-test_test <- process_data(test_raw, is_training_data = FALSE)
-train_test <- process_data(train_raw, is_training_data = TRUE)
+# feature engineering
+test <- process_data(test_raw, is_training_data = FALSE)
+train <- process_data(train_raw, is_training_data = TRUE)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+# data augmentation
+# note: do we want to consider adding test data into this? think about leakage...
+train <- train %>% bind_rows(augment_data(dat=train_raw, train))
