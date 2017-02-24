497b4b7135ba3bf4f8e5acbd890ccef32d12ab46
Eric Chang <ericchang00@gmail.com>
2017-02-24 15:00:18 -0600

    validated new data processing code

--- load_process_data.R
+++ load_process_data.R
@@ -8,54 +8,55 @@ source("dependencies.R")
 
 
 process_data <- function(dat, is_training_data){
-  # process data into form where id is primary key, extracting features:
-  # first event id, last event id, frequency counts of ids
-  
-  if (is_training_data) {
+    # process data into form where id is primary key, extracting features:
+    # first event id, last event id, frequency counts of ids
+    
+    if (is_training_data) {
     responses <- dat %>% group_by(id) %>% 
                          summarise(last_timestamp = last(timestamp), 
                                    response=last(event)) %>% 
                          mutate(response = as.factor(response))
     
     assert_that(all(table(dat$timestamp) == 1))  # getting response rows like this depends on timestamps being unique
-    row_is_response <- (dat$timestamp %in% responses$timestamp)
+    row_is_response <- (dat$timestamp %in% responses$last_timestamp)
+    responses <- select(responses, -last_timestamp)
     dat <- dat %>% filter(!row_is_response)
-  }
-  
-  # get first event, last event (response), and count of events
-  critical_events <- dat %>% group_by(id) %>% 
-    summarise(last_event = last(event),
-              first_event = first(event),
-              count_events = n()) %>% 
-    mutate(last_event = as.factor(last_event),
-           first_event = as.factor(first_event))
-  
-  # get frequency counts
-  count_events <- dat %>% 
-    group_by(id, event) %>% 
-    summarise(n = n()) %>% 
-    spread(event, n)
-  count_events[is.na(count_events)] <- 0
-  names(count_events) <- make.names(names(count_events))
-  
-  if (is_training_data) {
+    }
+    
+    # get first event, last event (response), and count of events
+    critical_events <- dat %>% group_by(id) %>% 
+        summarise(last_event = last(event),
+                  first_event = first(event),
+                  count_events = n()) %>% 
+        mutate(last_event = as.factor(last_event),
+               first_event = as.factor(first_event))
+    
+    # get frequency counts
+    count_events <- dat %>% 
+        group_by(id, event) %>% 
+        summarise(n = n()) %>% 
+        spread(event, n)
+    count_events[is.na(count_events)] <- 0
+    names(count_events) <- make.names(names(count_events))
+    
+    if (is_training_data) {
     # if we're making training data, join responses back in
-    critical_events <- left_join(critical_events, responses, by='id')
-  }
-  
-  return(left_join(critical_events, count_events))
+        count_events <- left_join(count_events, responses, by='id')
+    }
+    
+    return(left_join(critical_events, count_events))
 }
 
 
 augment_data <- function(dat, train, n_events){
-  # generates more training data by recalculating features
-  # on ids with n occured events by removing the nth observation 
-  # and setting event[n-1] as response. 
-  # iteratively "shaves" dataset down from max n_events to ids with only 2 events
-  out <- data.frame(names(train))
-  
-  
-  
+    # generates more training data by recalculating features
+    # on ids with n occured events by removing the nth observation 
+    # and setting event[n-1] as response. 
+    # iteratively "shaves" dataset down from max n_events to ids with only 2 events
+    out <- data.frame(names(train))
+    
+    
+
 }
 
 # MAIN
@@ -69,13 +70,13 @@ test_raw <- read.csv("data/test.csv")
 subm <- read.csv("data/sample_submission.csv")
 
 # move features from the training set to test set now
-test_raw <- left_join(test_raw, filter(train_raw, id %in% test_raw$id))
-train_raw <- filter(train_raw, !(id %in% test_raw$id))
+test_raw <- test_raw %>% left_join(filter(train_raw, id %in% test_raw$id))
+train_raw <- filter(train_raw, !(id %in% test_raw$id)) %>% mutate(id = as.character(id))
 assert_that(all(!train_raw$id %in% test_raw$id))  # assert no overlap between training and test sets
 
-# extract features
-test <- process_data(test_raw, is_training_data = FALSE)
-train <- process_data(train_raw, is_training_data = TRUE)
+# features engineering
+test_test <- process_data(test_raw, is_training_data = FALSE)
+train_test <- process_data(train_raw, is_training_data = TRUE)
 
 
 
